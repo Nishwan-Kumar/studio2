294a373c1a87d5263f8adfab81872fe87dcef8e1
Tricky Techy -தமிழ் <trickytechytamil12@gmail.com>
2025-08-25 03:00:37 +0000

    Change the firebase project name to blog page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Header } from '@/components/header';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Echo Chamber',
+  title: 'Blog Page',
   description: 'A place for thoughts and ideas to reverberate.',
 };
 
